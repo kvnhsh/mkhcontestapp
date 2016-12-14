85230bb7580c49e5b60a946fa4dd88b019d5d2ea
kvnhsh <kvn_hsh@yahoo.com>
2016-12-14 04:14:46 +0000

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Contestapp::Application.config.secret_key_base = '432d416602436a8b7846e03dc4d2a583cd8760f172dbdfaffa509701e69cb8ca30504613caf6f8d938106593fba7597c7f66e39b07ac7fb20e5d0c8d0f8c0d36'
